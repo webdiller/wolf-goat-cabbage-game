c00c1a63fd05ce0bcbca779f8568213c502facd0
Eugene Butkov <eugenefromrus@gmail.com>
2023-11-03 10:06:25 +1000

    feat: simple task

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,12 @@
-import '@/styles/globals.css'
-import type { AppProps } from 'next/app'
+import "@/styles/globals.css";
+import type { AppProps } from "next/app";
+import { HTML5Backend } from 'react-dnd-html5-backend'
+import { DndProvider } from 'react-dnd'
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Component {...pageProps} />
+  return (
+    <DndProvider backend={HTML5Backend}>
+      <Component {...pageProps} />
+    </DndProvider>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,118 +1,173 @@
-import Image from 'next/image'
-import { Inter } from 'next/font/google'
+import Image from "next/image";
+import { Inter } from "next/font/google";
+import mainImage from "@/public/main-image.jpg";
+import { useEffect, useState } from "react";
+import clsx from "clsx";
 
-const inter = Inter({ subsets: ['latin'] })
+const inter = Inter({ subsets: ["latin"] });
+
+interface Question {
+  title: React.ReactNode;
+  answers: Answer[];
+}
+interface Answer {
+  title: React.ReactNode;
+  isCorrect: boolean;
+}
 
 export default function Home() {
+  const question: Question = {
+    title: (
+      <p>
+        На одном берегу реки находятся крестьянин, волк, коза и капуста. У крестьянина есть лодка, но видимо не очень хорошая. Он может взять с собой в плавание только один «предмет», в смысле только
+        козу или только капусту или только волка. Проблема в том, что кое-кого нельзя оставлять наедине с желанной пищей. Например, нельзя уплыть оставив волка и козу на одном берегу – волк съест
+        козу. Или нельзя уплыть с волком, оставив козу и капусту – ведь коза съест капусту. Но крестьянину нужно непременно попасть на другой берег. Вот такой он упертый. И хочет он довезти всех в
+        сохранности.
+      </p>
+    ),
+    answers: [
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с капусты:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести капусту на правый берег.</li>
+              <li>Вернуться на левый берег без капусты.</li>
+              <li>Перевести козу на правый берег.</li>
+              <li>Взять капусту и вернуться на левый берег.</li>
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести капусту на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: false,
+      },
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с волка:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести козу на правый берег.</li>
+              <li>Взять волка и вернуться на левый берег.</li>
+              <li>Перевести капусту на правый берег.</li>
+              <li>Вернуться на левый берег без капусты.</li>
+              <li>Перевести волка на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: false,
+      },
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с козы:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести козу на правый берег.</li>
+              <li>Вернуться на левый берег без козы.</li>
+              <li>Перевести капусту на правый берег.</li>
+              <li>Взять козу и вернуться на левый берег.</li>
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести козу на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: false,
+      },
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с волка:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести капусту на правый берег.</li>
+              <li>Взять волка и вернуться на левый берег.</li>
+              <li>Перевести козу на правый берег.</li>
+              <li>Вернуться на левый берег без козы.</li>
+              <li>Перевести волка на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: false,
+      },
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с волка:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести козу на правый берег.</li>
+              <li>Взять волка и вернуться на левый берег.</li>
+              <li>Перевести капусту на правый берег.</li>
+              <li>Вернуться на левый берег без капусты.</li>
+              <li>Перевести волка на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: true,
+      },
+      {
+        title: (
+          <button>
+            <p className="text-bold uppercase mb-1">Начать с капусты:</p>
+            <ul className="list-decimal text-left list-inside">
+              <li>Перевести капусту на правый берег.</li>
+              <li>Вернуться на левый берег без капусты.</li>
+              <li>Перевести козу на правый берег.</li>
+              <li>Взять капусту и вернуться на левый берег.</li>
+              <li>Перевести волка на правый берег.</li>
+              <li>Вернуться на левый берег без волка.</li>
+              <li>Перевести капусту на правый берег.</li>
+            </ul>
+          </button>
+        ),
+        isCorrect: false,
+      },
+    ],
+  };
+  const [selected, setSelected] = useState<Answer | null>(null);
+  const onSelectAnswer = (answer: Answer) => setSelected(answer);
+
+  useEffect(() => {
+    if (selected && !selected.isCorrect) {
+      alert("Неверно!");
+      setSelected(null);
+    }
+    if (selected && selected.isCorrect) {
+      alert("Верно!");
+      setSelected(null);
+    }
+  }, [selected]);
+
   return (
-    <main
-      className={`flex min-h-screen flex-col items-center justify-between p-24 ${inter.className}`}
-    >
-      <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">src/pages/index.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <div className="container space-y-2 max-w-[800px] mx-auto p-4">
+      <Image width={mainImage.width} height={mainImage.height} alt="" className="w-full h-auto rounded-md object-cover" unoptimized src={mainImage.src} />
+      <div className="space-y-2">
+        <h1 className="text-2xl font-bold">Игра "Коза, капуста и волк"</h1>
+        <p>{question.title}</p>
+        <div className="grid grid-cols-2 gap-1">
+          {question.answers.map((item, indx) => {
+            return (
+              <div
+                key={indx}
+                onClick={() => onSelectAnswer(item)}
+                className={clsx("h-full text-left bg-transparent font-semibold hover:border-blue-500 hover:bg-blue-500 hover:text-white transition-all py-2 px-4 border rounded", {
+                  "order-blue-500 text-blue-700 border-blue-500": !selected,
+                  "border-red-500 text-red-700": selected?.isCorrect === false,
+                  "border-green-500 text-green-700": selected?.isCorrect === true && item.title === selected.title,
+                })}
+              >
+                {item.title}
+              </div>
+            );
+          })}
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700/10 after:dark:from-sky-900 after:dark:via-[#0141ff]/40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:max-w-5xl lg:w-full lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Discover and deploy boilerplate example Next.js&nbsp;projects.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=default-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  )
+    </div>
+  );
 }
